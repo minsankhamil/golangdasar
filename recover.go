be4428125cadcbec32a608d7cacb59aafdb68e2c
minsankhamil <khamil_insan0@yahoo.com>
2026-08-18 19:55:41 +0700

    learn golang (3/100) days

---
+++ recover.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func endApp() {
+	fmt.Println("End App")
+	message := recover()
+	fmt.Println("Terjadi Error", message)
+}
+
+func runApp(error bool) {
+	defer endApp()
+	if error {
+		panic("ERROR")
+	}
+}
+
+func main() {
+	runApp(true)
+}
