7dbc3e9f3b6970879137d27d7f9bbf3e5e4b2c77
minsankhamil <khamil_insan0@yahoo.com>
2026-08-18 12:55:06 +0700

    learn golang (3/100) days

---
+++ parameterfunction.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+type Filter func(string) string
+
+//function type declaration
+func sayHelloWithFilter(name string, filter Filter) {
+	fmt.Println("Hello ", filter(name))
+}
+
+func spamFilter(name string) string {
+	if name == "Anjing" {
+		return "***"
+	} else {
+		return name
+	}
+}
+
+func main() {
+	sayHelloWithFilter("Eko", spamFilter)
+
+	filter := spamFilter
+	sayHelloWithFilter("Anjing", filter)
+}
--- variadicfunction.go
+++ variadicfunction.go
@@ -11,7 +11,23 @@ func getCompleteName() (firstName, middleName, lastName string) {
 	return firstName, middleName, lastName
 }
 
+func sumAll(numbers ...int) int {
+	total := 0
+	//_ adalah indeks yang di blank kan
+	for _, number := range numbers {
+		total += number
+	}
+	return total
+}
+
 func main() {
 	firstName, middleName, lastName := getCompleteName()
 	fmt.Println(firstName, middleName, lastName)
+
+	total := sumAll(10, 10, 10, 10, 10)
+	fmt.Println(total)
+
+	numbers := []int{10, 10, 10}
+	total = sumAll(numbers...)
+	fmt.Println(total)
 }
